a2d4b4643e3a8ffb7c5f30ff8037d4fed47870ae
nicklusweti <nicklusweti@protonmail.com>
2024-07-23 18:12:00 +0300

    Basic home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <Button>
+        <Link href="/sign-up">Get Started</Link>
+      </Button>
+    </main>
+  );
 };
 export default Home;
